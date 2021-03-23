979bbd244b4a555e493c40141b21d3b39d0f301c
barrygg11 <barry01test@gmail.com>
2021-03-23 17:31:40 +0800

    model新增連線成功字串

--- model.php
+++ model.php
@@ -1,3 +1,12 @@
 <?php
 $db = mysqli_connect("localhost", "root", "Zx11111!", "cart") or die(mysqli_error());
+
+if (!$db) {
+    die('資料庫連結失敗!');
+} else {
+   echo '資料庫連結成功';
+}
+
+mysqli_query($db, "SET NAMES 'utf8'");
+
 ?>
