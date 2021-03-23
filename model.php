9fed1bfbd9a9a523a31c283305226acf17913f6d
barrygg11 <barry01test@gmail.com>
2021-03-23 17:34:23 +0800

    修改model內容第二次

--- model.php
+++ model.php
@@ -1,12 +1,6 @@
 <?php
 $db = mysqli_connect("localhost", "root", "Zx11111!", "cart") or die(mysqli_error());
 
-if (!$db) {
-    die('資料庫連結失敗!');
-} else {
-   echo '資料庫連結成功';
-}
-
 mysqli_query($db, "SET NAMES 'utf8'");
 
 ?>
